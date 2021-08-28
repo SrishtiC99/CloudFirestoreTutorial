efcf92a0bfaaea2eb048787934bd4cc3b8b5fe4d
Srishti Kumari <sri993951@gmail.com>
2021-08-28 12:49:03 +0530

    Added First Document to Firestore

--- app/src/main/java/com/example/srishti/cloudfirestore/MainActivity.java
+++ app/src/main/java/com/example/srishti/cloudfirestore/MainActivity.java
@@ -1,14 +1,69 @@
 package com.example.srishti.cloudfirestore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity";
+    private static String KEY_TITLE = "title";
+    private static String KEY_DESCRIPTION = "description";
+
+    private EditText titleET;
+    private EditText descriptionET;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titleET = findViewById(R.id.edit_text_title);
+        descriptionET = findViewById(R.id.edit_text_description);
+
+        findViewById(R.id.button_save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveNote();
+            }
+        });
+    }
+
+    public void saveNote(){
+        String title = titleET.getText().toString();
+        String description = descriptionET.getText().toString();
+
+        Map<String, Object> map = new HashMap<>();
+        map.put(KEY_TITLE, title);
+        map.put(KEY_DESCRIPTION, description);
+
+        db.collection("Notebook").document("Note 1").set(map)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(MainActivity.this, "Note Saved", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull @org.jetbrains.annotations.NotNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
     }
 }
